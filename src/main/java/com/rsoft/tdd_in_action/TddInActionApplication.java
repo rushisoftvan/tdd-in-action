68a2113b84f44a9075a965fe1828107d6195577d
rushikesh.softvan@gmail.com <rushikesh.softvan@gmail.com>
2025-05-01 17:20:31 +0530

    push the project

---
+++ src/main/java/com/rsoft/tdd_in_action/TddInActionApplication.java
@@ -0,0 +1,13 @@
+package com.rsoft.tdd_in_action;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddInActionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TddInActionApplication.class, args);
+	}
+
+}
